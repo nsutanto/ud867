c4bc2fd2785cf4bfcc93859062634c0c04aad271
Nicholas Sutanto <nsutanto@enova.com>
2018-08-24 11:08:37 -0500

    feat: Create Joke from Java

---
+++ FinalProject/javajokelib/src/main/java/com/udacity/nsutanto/javajokelib/JokeCreator.java
@@ -0,0 +1,8 @@
+package com.udacity.nsutanto.javajokelib;
+
+public class JokeCreator {
+
+    public static String createJoke() {
+        return "This is the Java Joke.. not funny at all";
+    }
+}
