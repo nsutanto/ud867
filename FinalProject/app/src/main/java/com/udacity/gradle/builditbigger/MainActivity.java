04ed70314c96edfc8c6ab2862cad1a21f5b8efee
Nicholas Sutanto <nsutanto@enova.com>
2018-08-24 11:08:09 -0500

    feat: Add Java Joke Library

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.nsutanto.javajokelib.JokeCreator;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        String javaJoke = JokeCreator.createJoke();
+        Toast.makeText(this, javaJoke, Toast.LENGTH_SHORT).show();
     }
 
 
