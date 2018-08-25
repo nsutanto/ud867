1c51be6c27bd563ecb277e76ce9104c9a1339411
Nicholas Sutanto <nsutanto@enova.com>
2018-08-25 10:00:54 -0500

    feat: Fix test case

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -10,7 +10,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static junit.framework.Assert.assertNotNull;
+import static android.text.TextUtils.isEmpty;
+import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
@@ -30,7 +31,6 @@ public class EndpointsAsyncTaskTest {
             e.printStackTrace();
         }
 
-        assertNotNull(result);
+        assertTrue(result, !isEmpty(result));
     }
-
 }
